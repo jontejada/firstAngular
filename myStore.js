e6b64924a885a4a1eee37ae773077ee211bec7b0
Jon Tejada <jon@jontejada.com>
2016-01-21 11:45:41 -0800

    store work

--- myStore.js
+++ myStore.js
@@ -27,14 +27,19 @@ angular.module("app", [])
 			$scope.totalCost = Math.floor(sum + (sum * taxRate));
 			console.log($scope.totalCost);
 		}
-
+		//shipping stuff
 		$scope.shippingMethods = [
-			{id:1, name:'ground', price:500},
-			{id:2, name:'air', price:2500},
-			{id:3, name:'overnight', price:4500}
+			{id:1, name:'ground', price:500, days:7},
+			{id:2, name:'air', price:2500, days:3},
+			{id:3, name:'overnight', price:4500, days:1}
 		];
 		$scope.selectedMethod = $scope.shippingMethods[0];
 
-
+		function deliveryDate() {
+			var date = new Date();
+			date.setDate(date.getDate() + 10);
+			console.log(date);
+		}
+		deliveryDate();
 
 	});
