dbfad7e733945ef51a7ef1cde29a4921856426ab
Jon Tejada <jon@jontejada.com>
2016-01-25 15:03:27 -0800

    color divs

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
 var app = angular.module('eventsApp', []);
 
-app.controller('EventsCon', function($scope) {
+app.controller('EventsCon', function($scope, $timeout) {
 	$scope.test = 24;
 	$scope.pickRandomNumber = function() {
 		$scope.test = Math.floor(Math.random() * 10) +1;
@@ -32,14 +32,43 @@ app.controller('EventsCon', function($scope) {
 	};
 	$scope.enterCount = 0;
 
+	$scope.colorHistory = [];
+
 	function randomColor() {
 	  var x=Math.round(0xffffff * Math.random()).toString(16);
 	  var y=(6-x.length);
 	  var z="000000";
 	  var z1 = z.substring(0,y);
 	  var color = "#" + z1 + x;
+	  $scope.colorHistory.push(color);
 	  return color;
 	}
 
+	$scope.colorDiv = '{background-color:white;}';
+	$scope.newColor = function () {
+		$scope.aColor = randomColor();
+	};
+
+	var replaying = false;
+	var replayCount = 0;
+
+	$scope.replay = function() {
+	  var displayPrevColor = function() {
+	  	replayCount++;
+	  	$scope.aColor = $scope.colorHistory[$scope.colorHistory.length - replayCount];
+	  	if ($scope.colorHistory.length  === replayCount) {
+	      replaying = false;
+	  	} else {
+	  		$timeout(displayPrevColor, 5);
+	  	}
+	  };
+
+	  if (!replaying) {
+	    replaying = true;
+	    // $timeout(function() { displayPrevColor(); }, 5000);
+	    displayPrevColor();
+	  }
+	  // displayPrevColor();
+	};
 
 });
