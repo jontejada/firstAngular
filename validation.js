604efe9be818e9054d645e74c87d268664814dad
Jon Tejada <jon@jontejada.com>
2016-01-27 16:46:13 -0800

    validation stuff

---
+++ validation.js
@@ -0,0 +1,12 @@
+var validationApp = angular.module("validationApp",[]);
+
+validationApp.controller("mainController",function($scope)
+	{ $scope.submitForm = function(isValid)
+		{if(isValid)
+			{
+				alert("our form is amazing");
+				// console.log($scope);
+			}
+		};
+	}
+);
